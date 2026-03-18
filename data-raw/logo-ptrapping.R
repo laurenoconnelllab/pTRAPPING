36a544112a26d153050212f473e35c38691e7d47
Camilo Rodriguez <camo.rodriguez@gmail.com>
2026-03-18 11:22:50 +0100

    Created logo

--- data-raw/logo-ptrapping.R
+++ data-raw/logo-ptrapping.R
@@ -1,23 +1,23 @@
 library(hexSticker)
 library(showtext)
 
-img <- "data-raw/frog_logo.png"
+img <- "data-raw/ptrapping_logo.png"
 
 ## Loading Google fonts (http://www.google.com/fonts)
 
-font_add_google("Patrick Hand", "patrick")
+font_add_google("Comic Neue", "comic")
 
 sticker(
   subplot = img,
-  package = "poisonfrogs",
-  p_color = "#404040ff",
-  p_size = 22,
-  p_y = 1.54,
-  p_family = "patrick",
+  package = "pTRAPPING",
+  p_color = "black",
+  p_size = 17,
+  p_y = 0.5,
+  p_family = "comic",
   p_fontface = "bold",
   filename = "man/figures/logo.png",
-  s_x = 1,
-  s_y = 1.07,
+  s_x = 0.99,
+  s_y = 1,
   s_width = .8,
   h_fill = "#f1f2f5ff",
   h_color = "#3a3a3aff"
